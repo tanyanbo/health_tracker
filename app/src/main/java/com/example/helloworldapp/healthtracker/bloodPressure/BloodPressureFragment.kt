e3e231b3d6231487a99466db877c1d64495988d9
Tan Yanbo <tanyanbo95@hotmail.com>
2021-04-09 01:04:53 +0800

    Revert "mistake"
    This reverts commit 3d5165922e3b10e342adca13171fc60b8974b30c.

--- app/src/main/java/com/example/helloworldapp/healthtracker/bloodPressure/BloodPressureFragment.kt
+++ app/src/main/java/com/example/helloworldapp/healthtracker/bloodPressure/BloodPressureFragment.kt
@@ -7,15 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import com.example.helloworldapp.healthtracker.R
 import com.example.helloworldapp.healthtracker.database.bloodPressure.BloodPressure
 import com.example.helloworldapp.healthtracker.databinding.FragmentBloodPressureBinding
 import com.example.helloworldapp.healthtracker.databinding.FragmentHeightWeightBinding
 import com.example.helloworldapp.healthtracker.viewModel.ViewModel
-import kotlinx.coroutines.launch
 
 class BloodPressureFragment : Fragment() {
 
@@ -27,32 +24,24 @@ class BloodPressureFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        binding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_blood_pressure, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_blood_pressure, container, false)
 
         val viewModelFactory = ViewModel.Factory(requireActivity().application)
         val viewModel =
             ViewModelProvider(requireActivity(), viewModelFactory).get(ViewModel::class.java)
 
-        adapter = BloodPressureRecyclerViewAdapter(object :
-            BloodPressureRecyclerViewAdapter.OnItemClickListener {
+        adapter = BloodPressureRecyclerViewAdapter(object: BloodPressureRecyclerViewAdapter.OnItemClickListener {
             override fun onItemClick(bp: BloodPressure) {
                 Toast.makeText(requireContext(), "delete button tapped", Toast.LENGTH_SHORT).show()
             }
         })
 
         binding.bloodPressureRecyclerView.adapter = adapter
-        viewModel.initializeData()
 
-        lifecycleScope.launch {
-            viewModel.job1.join()
-            viewModel.bloodPressureDataOnePerson.observe(viewLifecycleOwner, Observer {
-                adapter.submitList(it)
-            })
-        }
+
 
         return binding.root
     }
+
+
 }
-
-
