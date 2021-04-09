7c60a16f80ffb150bc46e15fb436691b1320375c
Tan Yanbo <tanyanbo95@hotmail.com>
2021-04-09 13:01:10 +0800

    implemented full logic for recycler view, add data, add person and view data for blood pressure, handled all known errors

--- app/src/main/java/com/example/helloworldapp/healthtracker/bloodPressure/BloodPressureFragment.kt
+++ app/src/main/java/com/example/helloworldapp/healthtracker/bloodPressure/BloodPressureFragment.kt
@@ -1,6 +1,7 @@
 package com.example.helloworldapp.healthtracker.bloodPressure
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,11 +10,14 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.helloworldapp.healthtracker.R
+import com.example.helloworldapp.healthtracker.addPerson.AddPersonFragment
 import com.example.helloworldapp.healthtracker.database.bloodPressure.BloodPressure
 import com.example.helloworldapp.healthtracker.databinding.FragmentBloodPressureBinding
 import com.example.helloworldapp.healthtracker.databinding.FragmentHeightWeightBinding
 import com.example.helloworldapp.healthtracker.viewModel.ViewModel
+import kotlinx.coroutines.launch
 
 class BloodPressureFragment : Fragment() {
 
@@ -25,13 +29,15 @@ class BloodPressureFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_blood_pressure, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_blood_pressure, container, false)
 
         val viewModelFactory = ViewModel.Factory(requireActivity().application)
         val viewModel =
             ViewModelProvider(requireActivity(), viewModelFactory).get(ViewModel::class.java)
 
-        adapter = BloodPressureRecyclerViewAdapter(object: BloodPressureRecyclerViewAdapter.OnItemClickListener {
+        adapter = BloodPressureRecyclerViewAdapter(object :
+            BloodPressureRecyclerViewAdapter.OnItemClickListener {
             override fun onItemClick(bp: BloodPressure) {
                 Toast.makeText(requireContext(), "delete button tapped", Toast.LENGTH_SHORT).show()
             }
@@ -39,9 +45,23 @@ class BloodPressureFragment : Fragment() {
 
         binding.bloodPressureRecyclerView.adapter = adapter
 
-        viewModel.bloodPressureAllDataOnePerson.observe(viewLifecycleOwner, Observer {
-            adapter.submitList(it)
-        })
+        lifecycleScope.launch {
+
+            viewModel.setAllData()
+            viewModel.job2.join()
+            Log.i("MainActivity", "blood pressure fragment navigateToAddPerson: ${viewModel.navigateToAddPerson.value}")
+            try {
+                viewModel.bloodPressureAllDataOnePerson.observe(viewLifecycleOwner, Observer {
+                    adapter.submitList(it)
+                })
+            } catch (e: UninitializedPropertyAccessException) {
+                parentFragmentManager.beginTransaction().apply {
+                    replace(R.id.frameLayout, AddPersonFragment())
+                    commit()
+                }
+            }
+        }
+
 
         return binding.root
     }
